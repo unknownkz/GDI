fc2c9076fc8d114baa1519201a3fde94523c4a4b
ZokoToozNoob001 <78256222+ZokoToozNoob001@users.noreply.github.com>
2022-02-27 11:42:13 +0630

    2.0.22mod

--- worker/worker-generator.js
+++ worker/worker-generator.js
@@ -7,7 +7,7 @@ const clientId = '746239575955-oao9hkv614p8glrqpvuh5i8mqfoq145b.apps.googleuserc
 const clientSecret = 'u5a1CSY5pNjdD2tGTU93TTnI';
 const grantType = 'authorization_code';
 const redirectURI = 'urn:ietf:wg:oauth:2.0:oob';
-const jsURL = 'https://gitlab.com/ParveenBhadooOfficial/Google-Drive-Index/-/raw/master/worker/worker-super.js';
+const jsURL = 'https://raw.githubusercontent.com/ZokoToozNoob001/GDI/master/worker/worker-super.js';
 
 async function handleRequest(request) {
     let title = getParameterByName(request.url, 'site_name') || 'Bhadoo Drive Index';
@@ -45,7 +45,7 @@ async function handleRequest(request) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
-    <title>Bhadoo Drive Index - Beta</title>
+    <title>Zoko's Drive Index</title>
     <link rel="icon" type="image/x-icon" href="https://www.cloudflare.com/favicon.ico"/>
     <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.0.0/dist/slate/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
@@ -284,7 +284,7 @@ async function handleRequest(request) {
 <body">
 <div class="container">
     <br><br>
-    <a href="https://www.npmjs.com/package/@googledrive/index"><h3>Bhadoo Index Generator - Beta</h3></a>
+    <a href="https://www.npmjs.com/package/@googledrive/index"><h3>Zoko's Index Generator</h3></a>
     <br>
         <div>
             <h3>1. <a
